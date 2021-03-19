f0fa77ebe294bf9d07ef61793bfb6a8a57648594
AahzBrut <partyomenko@gmail.com>
2021-03-19 20:31:27 +0300

    Add pellet counting system.

---
+++ Assets/Scripts/Systems/GameStateSystem.cs
@@ -0,0 +1,17 @@
+using Components;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Systems
+{
+    [AlwaysUpdateSystem]
+    public class GameStateSystem : SystemBase
+    {
+        protected override void OnUpdate()
+        {
+            var pelletQuery = GetEntityQuery(ComponentType.ReadOnly<Pellet>());
+            
+            Debug.Log($"Number of pellets left: {pelletQuery.CalculateEntityCount()}");
+        }
+    }
+}
